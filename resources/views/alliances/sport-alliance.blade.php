dabc2d9ae03f8a7216d36d87bd8282c536f5ac1e
peter0512lee <peter0512lee@gmail.com>
2024-03-20 00:57:44 +0800

    update alliance

--- resources/views/alliances/sport-alliance.blade.php
+++ resources/views/alliances/sport-alliance.blade.php
@@ -1,6 +1,6 @@
 @extends('layout.master')
 
-@section('title', 'rrclp-alliance')
+@section('title', 'sport-alliance')
 
 @section('extra_head')
 <script src="/js/rellax.min.js"></script>
@@ -18,25 +18,24 @@
 <section id="introduce" class="intro">
     <label>Alliance</label>
     <h1>智能運動科技應用技術聯盟</h1>
-    <p>Extended Reality Application Alliance</p>
+    <p>Intelligent Sports Technology Application Alliance</p>
 
     <div class="title">
         <h1>聯盟介紹</h1>
         <div class="under"></div>
         <p class="context">
-            為整合國內與校內外跨系所之資訊科技與創新服務研發等相關研究資源，
-            進行跨領域研究團隊組成與合作、全面性創新資訊服務應用產業界所殷切需求之人才培育、
-            技術與產品創新研發以協助企業解決問題，進而加乘研發成果的潛在價值，以建立本校獨特特色、
-            提升國內創新資訊服務應用產業人員技術及研發能力與創新能量、促進產業升級並引領國際，
-            以提升國家的經濟發展之整體競爭力，爰設立本聯盟。
+            本聯盟進行運動科技跨領域研究團隊之組成與合作，
+            整合國立清華大學運動科技中心校內跨系所資源與友校國立臺灣科技大學數位內容研發中心跨校資源，
+            共同培育運動科技應用產業界所殷切需求之人才，研發創新技術與產品，協助企業解決問題，
+            進而加乘研發成果的潛在價值，並提升國內創新運動科技應用產業人員技術及研發能力，以建立聯盟獨特特色。
         </p>
         <p class="context">
-            我們將匯集產學資源，聯合業界指標性公司，並整合各專長領域教授、
-            研究人員組成菁英培育實驗室進行尖端、實用之產業相關之研發工作；
-            計有第一類別：遊戲設計創新、遊戲互動模式、遊戲內容自動化生成、
-            遊戲人工智慧、遊戲系統架構、遊戲繪圖與引擎技術開發領域；
-            第二類別：AR、VR、MR與相關之電腦視覺、人機互動、機電整合系統等核心應用技術的開發領域；
-            第三類別：大數據分析、電腦繪圖學、電腦視覺、人工智慧、深度學習、網路計算等核心基礎技術領域。
+            我們將整合團隊成員所累積之創新研發能量，促進產業升級並與國際接軌，
+            以提升國家的運動科技經濟發展整體競爭力，相關研發成果亦將進一步導入健康照護產業。
+            我們將匯集產學資源，聯合業界指標性公司與各專長領域教授、研究人員組成菁英培育實驗室進行尖端、
+            實用之產業相關研發工作，包含智慧運動場館基礎建設（感測器部署、場館搭建、資料蒐集平台建置）、
+            應用技術開發（XR技術、電腦視覺技術、電腦圖學技術）、
+            其他核心技術應用（大數據分析、資料科學、人工智慧、深度學習、機器學習）。
         </p>
     </div>
 </section>
@@ -48,108 +47,153 @@
     <div class="under"></div>
 
     <a id="select-title" onclick="select()">
-        虛實整合
+        影像分析輔助運動戰技解析
         <img src="/images/button_down.svg">
     </a>
     <ul id="select-list" class="tab">
-        <li><a class="active" data-target="#adv1" onclick="swTab(this)">虛實整合</a></li>
-        <li><a data-target="#adv2" onclick="swTab(this)">創新人機互動</a></li>
-        <li><a data-target="#adv3" onclick="swTab(this)">實景重建技術</a></li>
-        <li><a data-target="#adv4" onclick="swTab(this)">VR互動</a></li>
+        <li><a class="active" data-target="#adv1" onclick="swTab(this)">影像分析輔助運動戰技解析</a></li>
+        <li><a data-target="#adv2" onclick="swTab(this)">延展實境輔助運動訓練</a></li>
+        <li><a data-target="#adv3" onclick="swTab(this)">感測器輔助運動訓練</a></li>
+        <li><a data-target="#adv4" onclick="swTab(this)">虛實整合輔助運動娛樂</a></li>
     </ul>
 
     <div class="content">
         <div id="adv1" class="tab-c show">
 
-            <h2>虛實合成</h2>
+            <h2>籃球投籃分析</h2>
             <div class="context">
-                隨著電腦繪圖能力的進步，近期在深度學習領域興起一股在模擬環境(Simulation)中執行深度神經網路訓練與驗證的潮流，
-                相對於由全虛擬物件產生的模擬結果，將現實影像當作場景並合成虛擬物件的虛實合成技術，不但大幅降低場景產生與變更的成本，
-                現實影像更多的細節，也有助於深度學習學習到更接近使用情境的資料，另一方面，部分虛擬的物件也可以維持大量產生極端測資該項使用模擬環境的優點。
+                提出一套精準籃球投籃分析系統，可應用於比賽分析、球員訓練和精彩片段生成。
+                給定籃球比賽影片作為輸入，該系統首先偵測/切割候選投籃片段，
+                然後即時地分析每個投籃候選片段的「投籃類型」、「投籃結果」和「球的狀態」。
+                該方法有效運用球場物件的相對時空關聯性，與現有分析籃球投籃的方法相比，
+                該算法可以更好地處理攝影機任意移動的影片，同時提高準確性。
             </div>
             <div class="media-ctext">
-                <iframe class="VideoFrame" src="https://www.youtube.com/embed/u7EViqJzn9I" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                <iframe class="VideoFrame" src="https://www.youtube.com/embed/BH85gCksKu4" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                <iframe class="VideoFrame" src="https://www.youtube.com/embed/tAY4gIYRazk" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                <img src="/images/SPORT/shoot_analysis.png" class="Mask">
             </div>
 
-            <h2>360影像虛擬光源合成</h2>
+            <h2>籃球戰術分析</h2>
             <div class="context">
-                由於人眼對於影像中光線資訊的變化是十分敏感，因此，不論是常見的擴增實境或是更進階的虛實整合，
-                虛擬物件與現實場景光源的統一，都是結果是否真實十分重要的指標，
-                藉由人工智慧偵測360影像中光源資訊，並可及時套用於虛擬物件上製造出正確的光影效果，甚至部分材質上有著環境的反射等，
-                使虛擬物件能更加真實的與影像中場景合而為一。
+                基於鳥瞰軌跡或2D影像熱圖的籃球進攻戰術辨識及防守球員軌跡生成系統，
+                由於目前的戰術識別方法大多基於鳥瞰軌跡訊息來學習球員的時空相關性，為了獲得正確的鳥瞰球員軌跡，
+                強大的相機校準和球員追蹤技術是必不可少的。然而，對於攝影機來說，運動較大、球員遮擋嚴重、
+                球員球衣相似的轉播影片，獲得準確的攝影機參數和球員追蹤結果具有很大的挑戰性，導致戰術分析性能較差。
+                因此本研究亦設計一套不須應用攝影機校準和球員追蹤技術的戰術識別方法，
+                在推理階段直接從2D攝影機影像的球員熱圖預測戰術類別。
+                該方法達到了與當前依賴於完美鳥瞰圖軌跡輸入的戰術分類方法相當的精度。
             </div>
             <div class="media-ctext" style="justify-content: space-around;">
-                <img src="/images/ERA/non_ibr.jpg" class="Mask">
-                <img src="/images/ERA/ibr.jpg" class="Mask">
+                <img src="/images/SPORT/tactical_analysis.jpg" class="Mask" style="width: 100%; height: auto;">
+            </div>
+
+            <h2>關鍵球員分析與事件偵測</h2>
+            <div class="context">
+                提出一個可執行即時事件偵測及關鍵人物分析的系統，提供球隊進行情報蒐集等應用。
+                當今運動賽事正蓬勃發展，許多球隊為贏得比賽，皆透過情報蒐集了解對手優劣勢，以制定相應的戰術策略。
+                然而，僅依靠基礎數據表(如整場比賽得分、各球員事件數量) 無法獲取球員比賽時較細節的資訊，
+                使得球隊經常會聘請專業的分析師，藉由回放影片的方式重新定位事件發生的時間點與球員動態。
+                這樣的方式極度耗費人力成本，因此團隊提出混合式方法，在兼顧執行速度的同時也能達到足夠的準度，
+                透過粗略事件切分模組以及關鍵人物偵測模組，來達到初步的結果預測，之後使用該兩者資訊獲得最後精確之結果，
+                包含事件種類及發生時間、對應該事件之動作人物以及關鍵人物資訊。
+            </div>
+            <div class="media-ctext" style="justify-content: space-around;">
+                <img src="/images/SPORT/key_player.png" class="Mask" style="width: 100%; height: auto;">
+            </div>
+
+            <h2>即時投籃熱點分析</h2>
+            <div class="context">
+                提出一個可利用球場視角影像來分析投籃事件與投籃位置的即時投籃熱點分析系統，
+                其分析的投籃事件包含兩分球、三分球、罰球事件。近年來，資訊技術已成為各種運動賽事中必不可少的一部分，
+                藉由攝影機擷取場上的資訊，並透過電腦視覺和人工智慧技術對影片進行分析，使運動賽事可以提供更豐富的運動數據。
+                這些數據的分析和應用，可以讓運動賽事在不同方面上得到更多的發展和進步。團隊提出一個將轉播視角相機拍攝之籃球影像，
+                透過深度神經網路模組來預測投籃事件和投籃位置的系統。
+            </div>
+            <div class="media-ctext" style="justify-content: space-around;">
+                <iframe class="VideoFrame" src="https://www.youtube.com/embed/gA2JbanK05c" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                <img src="/images/SPORT/omniscorer.png" class="Mask">
             </div>
         </div>
 
         <div id="adv2" class="tab-c">
-            <h2>智慧手套</h2>
+            <h2>戰術訓練</h2>
             <div class="context">
-                人機互動科技在電腦科學中都是十分熱門的領域，從最原始的鍵盤滑鼠，到目前最新穎的Kinect，
-                足可知道人機互動在電腦科學中一直都有舉足輕重的地位，而穿戴科技則是人機互動下一個重要發展指標，
-                隨著google 眼鏡及各式各樣的健康手環上市，穿戴科技已經慢慢地開始萌芽，因此現階段將是切入的最好時機點。
-                有鑒於此，我們希望能將穿戴科技作更廣泛的延伸，主持人利用低功率的中央控制器，實作出成本低廉的互動式手套。
+                提出一個基於虛擬實境的戰術訓練系統，以提高球員理解進攻戰術的能力並幫助其正確練習戰術。
+                教練可透過電子戰術板繪製戰術，系統自動將二維的戰術軌跡資訊轉換為三維的虛擬球員動畫，
+                透過虛擬實境頭盔即可由任意視角觀看虛擬球員在球場上跑動的過程。
+                使用者可使用第一人稱視角從某一特定球員的視角觀戰戰術跑動時的細節資訊
+                (例如視野範圍內其他隊友何時何地會有空檔機會、當隊友幫忙擋人時自己該如何跑動才能在隊友幫忙製造出的有限空間中順利被掩護)，
+                也可使用第三人稱視角觀看戰術全局如何跑動，提供不同程度沉浸感的訓練效果。
+                同時，為了驗證使用沉浸式頭盔進行戰術教學與訓練能比用傳統戰術板的方法得到更好的學習成效，
+                也開發輕量化的多攝影機球員追蹤系統，記錄和分析每個球員在戰術執行過程中的軌跡。
+                基於多項主觀量測(包括模擬器暈動症、存在感和運動意象能力)，我們已驗證高沉浸式戰術訓練可以提高運動員的戰略想像能力。
             </div>
             <div class="media-ctext">
-                <iframe class="VideoFrame" src="https://www.youtube.com/embed/ebXD7CzD__g" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                <img src="/images/ERA/gloves_news.jpg" class="Mask">
+                <iframe class="VideoFrame" src="https://www.youtube.com/embed/-9uvrFCxy8Y" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                <img src="/images/SPORT/Training-Assistant.png" class="Mask">
             </div>
-            <h2>數位VR牙醫訓練評測系統</h2>
+            <h2>視野訓練</h2>
             <div class="context">
-                該專案目的在於製作牙醫訓練虛擬裝置，將現實中的手部動作與手機位置真實反映在虛擬實境中。
-                由於一般控制器只能在虛擬環境中表現其粗略的位置與轉向，
-                無法真實地反映手指彎曲、伸張等細微的動作，亦無法完全將抓取、觸摸物體時物體的反應體現，
-                而牙科訓練對於手勢、手指擺放位置與手指用力程度等都必須有適當要能偵測與反應，
-                因此，結合上述「智慧手套」之技術進行手勢姿態的抓取，再利用磁力定位來捕捉牙科訓練時的細微操作，
-                並反應於虛擬實境中，並提供自動評分、紀錄、多視角回放等功能，協助牙醫訓練教學。
+                提出了一套沉浸式籃球視野訓練系統，不僅訓練球員的視野，還要求球員穩定運球，模仿真實籃球比賽的情況。
+                於運動員來說，視野訓練是贏得體育比賽的關鍵之一，傳統的視覺訓練方法適合只專注於球的運動。
+                然而，對於籃球來說，球員需要在球場上同時觀察多個移動物體（即球和球員）。
+                該系統允許使用者配戴虛擬實境頭盔時，可直接基於手勢和語音(而不是控制器)進行系統操作，
+                更直觀地與系統互動。系統更可根據受訓者選擇的訓練配置來模擬訓練場景，
+                並蒐集使用者訓練當下的影片及其於虛擬實境中的眼球運動軌跡，藉此分析受訓者的目光專注區域、運球動作和運球次數，
+                進而分析使用者視野訓練之成效。
             </div>
             <div class="media-ctext">
-                <iframe class="VideoFrame" src="https://www.youtube.com/embed/sHnY4XIWwhA" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                <iframe class="VideoFrame" src="https://www.youtube.com/embed/EQ0i3dVtB9g" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                <img src="/images/SPORT/vision-training.png" class="Mask">
+            </div>
+            <h2>進攻動作訓練</h2>
+            <div class="context">
+                提出了一種利用虛擬實境和動作辨識的籃球動作感知進攻決策訓練系統。動作決策在籃球進攻中有著至關重要的作用，
+                進攻球員必須在各種防守情況下做出有效的動作決策才能較容易進攻得分。我們創建可控制、
+                可重複練習的訓練場景來增強運動員做出最佳決策的能力。使用者於虛擬實境中將觀看不同的虛擬防守者防禦情境，
+                並需要即時做出對應的進攻動作，系統會根據使用者所穿戴的感測器裝置來辨識其進攻動作為何，如做出錯誤決策時會收到決策建議。
+            </div>
+            <div class="media-ctext">
+                <img src="/images/SPORT/offensive-training.png" class="Mask" style="width: 100%; height: auto;">
+            </div>
+            <h2>拳擊距離訓練</h2>
+            <div class="context">
+                提出了一套專為提升拳擊距離感而設計的虛擬實境訓練系統，藉由與拳擊專業人士進行需求訪談後，
+                了解拳擊比賽的致勝關鍵在於擁有良好的距離感以觀察對手出拳的時機、距離、角度與速度，並做出相對應的反應。
+                為了克服現有距離感訓練方式之空間與時間的限制，引入虛擬實境技術，設計並整合了各種視覺和聽覺輔助工具，
+                並提供豐富的反饋機制。團隊也將此虛擬實境拳擊距離訓練系統提供給專業的拳擊教練體驗，
+                驗證該系統能被融入日常訓練中，提供選手進行自主訓練，從而提高拳擊選手的整體訓練效果和表現。
+            </div>
+            <div class="media-ctext">
+                <img src="/images/SPORT/boxing.png" class="Mask" style="width: 100%; height: auto;">
             </div>
         </div>
 
         <div id="adv3" class="tab-c">
-            <h2>Tango</h2>
+            <h2>裁判動作訓練</h2>
             <div class="context">
-                Tango手機藉由深度的資訊與區域學習(Area Learn)機制將實景重建成3D模型，
-                藉由這些3D資訊可以讓程式更加理解現實的場域，從而達到精準且穩定的定位，
-                甚至讓虛擬物件可與現實物件進行互動而達到混合實境(Mixed Reality，MR)的效果。
+                提出了一套基於多個慣性測量單元（IMU）感測器的動作辨識方法，
+                透過感測器可以記錄肢體的加速度和角加速度訊號，並加以分析理解其動作為何。
+                該系統採用了籃球官方裁判信號，高達65種手勢，包括大動作手部動作和細微動作手部動作，
+                來進行動作辨識任務。經由實驗也證實該系統可以有效地增強使用者的裁判動作判決能力。
             </div>
             <div class="media-ctext">
-                <iframe class="VideoFrame" src="https://www.youtube.com/embed/qYLpczJ8x2w" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                <iframe class="VideoFrame" src="https://www.youtube.com/embed/pHHy4MPvR9s" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-            </div>
-
-            <h2>360室內場景重建(DuLa Net)</h2>
-            <div class="context">
-                過去幾年WebGL技術的成熟賦予了網頁顯示3D模型的能力，並大量被運用到展示或導覽等場域，
-                其中3D模型需要花費大量的人力與時間來建置，像是室內導覽就需要根據場域各自建模，無疑提高整體成本，
-                該技術利用深度學習技術賦予現實影像3D空間資訊藉此自動產生室內空間模型，為後續各式互動快速提供穩固的基礎。
-            </div>
-            <div class="media-ctext">
-                <img src="/images/ERA/CVPR_2019_DuLa-Net.jpg" class="Mask">
-                <iframe class="VideoFrame" src="https://www.youtube.com/embed/laBktb51t48" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                <img src="/images/SPORT/referee.png" class="Mask" style="width: 100%; height: auto;">
             </div>
         </div>
 
         <div id="adv4" class="tab-c">
-            <h2>VR互動遊戲</h2>
+            <h2>虛擬廣告擴增編輯器</h2>
             <div class="context">
-                虛擬實境(Virtual Reality)隨著硬體的逐漸改善，除了沉浸式的觀賞體驗，更提供全新的互動模式，
-                你可以坐在高級跑車中享受街頭賽車的刺激，也可以穿越歷史或橫渡海洋來趟生態人文之旅，盡情享受各式奇幻的冒險，
-                這也使得遊戲的操作不再侷限於按按鍵盤、動動滑鼠，藉由各式定位系統與客製化的輔具，可以真實的與場景中的3D物件互動，
-                提供更佳的遊戲體驗。
+                提出了一套可針對運動影片進行虛擬廣告編輯的系統，為了讓使用者更好地操作此編輯器，
+                將編輯器移植至網頁系統，同時針對功能部分新增了素材清單，
+                讓使用者可以透過拖放方式將素材庫中的圖片新增到畫面中，也於編輯介面讓使用者能夠調整圖片的大小、
+                位置和旋轉等參數，以便更符合使用者需求來放置圖片。由於系統中新增了廣告看板和觀眾區域，
+                因此在放置圖片時，會根據各區域的重建參數進行擺放，使圖片能夠與各區塊的平面貼合得更為自然。
+                透過該系統，即可在一般運動影片內置入不過於突兀的3D虛擬廣告。
             </div>
             <div class="media-ctext">
-                <iframe class="VideoFrame" src="https://www.youtube.com/embed/XNrWYUgR0ug" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                <iframe class="VideoFrame" src="https://www.youtube.com/embed/cVGK_bmyscU" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-            </div>
-            <div class="media-ctext">
-                <iframe class="VideoFrame" src="https://www.youtube.com/embed/oXOvYbWgBH0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                <iframe class="VideoFrame" src="https://www.youtube.com/embed/RSSCCOlQpr0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                <img src="/images/SPORT/virtual.png" class="Mask" style="width: 100%; height: auto;">
             </div>
         </div>
     </div>
@@ -164,40 +208,13 @@
 
     <ul class="grid">
         <li class="item">
-            <img src="/images/member/Yao.jpg">
+            <img src="/images/member/MCHu.jpg">
             <div class="left-line">
                 主持人
-                <span>姚智原</span>
+                <span>胡敏君</span>
             </div>
-            <div> 臺科大資工系 副教授 </div>
-            <div> 電腦圖學、電腦視覺、延展實境、人機互動、醫療影像3D重建 </div>
-        </li>
-        <li class="item">
-            <img src="/images/member/Lin.jpg">
-            <div class="left-line">
-                共同主持人
-                <span>林士勛</span>
-            </div>
-            <div> 海大資工系 助理教授 </div>
-            <div> 電腦繪圖、資訊視覺化、圖形識別 </div>
-        </li>
-        <li class="item">
-            <img src="/images/member/Lai.jfif">
-            <div class="left-line">
-                共同主持人
-                <span>賴祐吉</span>
-            </div>
-            <div> 臺科大資工系 副教授 </div>
-            <div> 電腦遊戲技術、電腦圖學、電腦影像生成、電腦動畫、3D模型重建、電腦視覺 </div>
-        </li>
-        <li class="item">
-            <img src="/images/member/Hua.jfif">
-            <div class="left-line">
-                共同主持人
-                <span>花凱龍</span>
-            </div>
-            <div> 臺科大資工系 教授 </div>
-            <div> 多媒體大數據、深度學習、電腦視覺、行動多媒體應用、APP 程式開發 </div>
+            <div> 清華資工系 教授 </div>
+            <div> 電腦圖學、電腦視覺、延展實境、人機互動 </div>
         </li>
         <li class="item">
             <img src="/images/member/Chu.jpg">
@@ -205,8 +222,17 @@
                 共同主持人
                 <span>朱宏國</span>
             </div>
-            <div> 清華資工系 副教授 </div>
+            <div> 清華資工系 教授 </div>
             <div> 計算機圖學、三維模型處理、貼圖紋理合成、視覺感知與應用 </div>
+        </li>
+        <li class="item">
+            <img src="/images/member/Pan.png">
+            <div class="left-line">
+                共同主持人
+                <span>潘則佑</span>
+            </div>
+            <div> 臺科大資工系 助理教授 </div>
+            <div> 多媒體內容分析、電腦視覺、人機互動、延展實境 </div>
         </li>
     </ul>
 </section>
@@ -313,32 +339,6 @@
             </ul>
             <div class="price">NT$ <span>10</span> 萬元整</div>
         </div>
-        <!-- <div class="level">
-            <a class="lv-title" onclick="coll_right(this)">
-                <picture>
-                    <source srcset="/images/alliance/diamond.svg" media="(min-width: 576px)">
-                    <source srcset="/images/alliance/diamond-m.svg" media="(max-width: 576px)">
-                    <img src="/images/alliance/diamond.svg" class="lv-icon">
-                </picture>
-                <span>鑽石會員</span>
-                <picture>
-                    <source srcset="/images/empty.png" media="(min-width: 576px)">
-                    <source srcset="/images/button_plus.svg" media="(max-width: 576px)">
-                    <img src="" class="lv-toggle">
-                </picture>
-            </a>
-            <ul>
-                <li>產學合作服務</li>
-                <li>提供線上產學平台(企業需求提案)</li>
-                <li>參與技術論壇/研討會(免報名費)</li>
-                <li>舉辦專屬攬才說明會(寒假、暑假各一次)</li>
-                <li>產碩專班人才培育合作(1個免費名額)</li>
-                <li>協助政府相關產學計畫提案合作</li>
-                <li>每年依公司政策推薦合適教授媒合</li>
-                <li>專人PM服務</li>
-            </ul>
-            <div class="price">NT$ <span>65</span> 萬元整</div>
-        </div> -->
     </div>
 </section>
 
